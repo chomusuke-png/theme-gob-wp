58a551cafbafa4a39d6d5e1ab13fedea6e7a5878
chomusuke-png <owokenji@gmail.com>
2025-12-19 17:11:34 -0300

    yugan de mil kilowatt

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ get_header();
 
 <main id="primary" class="site-main">
 
-    <!-- <section class="hero-gov-section">
-        
-    </section> -->
-
     <?php if ( is_active_sidebar( 'home-widgets' ) ) : ?>
         <section class="home-widgets-section">
             <div class="container">
