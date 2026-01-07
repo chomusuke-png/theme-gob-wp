f7b28d8737e8e9b83a2ac6d4b2fd200c10e6c59f
chomusuke-png <owokenji@gmail.com>
2026-01-07 15:19:12 -0300

    ajustes visuales al index

--- index.php
+++ index.php
@@ -4,17 +4,15 @@ get_header();
 
 <main id="primary" class="site-main">
 
-    <?php get_template_part( 'template-parts/home', 'hero' ); ?>
+    <div class="container">
+        <?php get_template_part( 'template-parts/home', 'hero' ); ?>
 
-    <?php get_template_part('template-parts/home', 'video'); ?>
-
-    <?php if ( is_active_sidebar( 'home-widgets' ) ) : ?>
-        <section class="home-widgets-section">
-            <div class="container">
+        <?php if ( is_active_sidebar( 'home-widgets' ) ) : ?>
+            <section class="home-widgets-section">
                 <?php dynamic_sidebar( 'home-widgets' ); ?>
-            </div>
-        </section>
-    <?php endif; ?>
+            </section>
+        <?php endif; ?>
+    </div>
 
 </main>
 
