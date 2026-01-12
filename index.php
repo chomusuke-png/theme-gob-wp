755f1463dd593c8c861b94765ee71ff5a8537ae4
chomusuke-png <owokenji@gmail.com>
2026-01-12 10:47:57 -0300

    1

--- index.php
+++ index.php
@@ -3,10 +3,11 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-
     <div class="container">
         <?php get_template_part( 'template-parts/home', 'hero' ); ?>
+    </div>
 
+    <div class="container">
         <?php if ( is_active_sidebar( 'home-widgets' ) ) : ?>
             <section class="home-widgets-section">
                 <?php dynamic_sidebar( 'home-widgets' ); ?>
